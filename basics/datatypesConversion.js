fc55954f62b438cd49aadfe791820587197f3cd4
indu045 <indujawla2005@gmail.com>
2025-06-19 07:07:24 +0000

    datatypeConversion and operations

--- basics/datatypesConversion.js
+++ basics/datatypesConversion.js
@@ -46,3 +46,35 @@ let someNumber = 389
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof (stringNumber))//string
+
+
+
+ //OPERATIONS
+console.log(2+2)//addition operation
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) //power operation
+console.log(3/9)
+console.log(2%3)//remindar
+
+let str1 = "hello"
+let str2 ="world"
+let str3 = str1 + " "+ str2;
+console.log(str3) //hello world
+
+//problems (don't write in this way)
+console.log("1" +2)//12
+console.log(1+"2")//12
+console.log("1" + 2+2)//122
+console.log(1+2+"2")//32
+
+console.log(+true) //1
+console.log(+"")//0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter; //read the difference
+console.log(gameCounter)
