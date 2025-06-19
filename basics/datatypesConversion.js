d07e9cc3d0cd75b6f0c6aab4945cd4c41d49ad74
indu045 <indujawla2005@gmail.com>
2025-06-19 06:54:17 +0000

    conversion of datatypes

---
+++ basics/datatypesConversion.js
@@ -0,0 +1,48 @@
+let score = 54 //defined on the frontend
+let score2 = "34"
+//but if you are only working on backend
+// to know it's datatype
+// const {score} = req.body;
+let score3 = "33ad"
+console.log(typeof score) //number
+
+console.log(typeof(score2)) //string
+
+
+// to convert score2 into number
+let valueInNumber = Number(score2) 
+console.log(typeof valueInNumber) //number
+
+let valueInNumber2 = Number(score3) 
+console.log(typeof valueInNumber2)//number
+console.log(valueInNumber2)//NaN as score3 = 33ad is not a pure number
+
+// to convert boolean
+let istrue = true
+let istrue2 = false
+let booleanInNumber= Number(istrue2) 
+console.log(typeof istrue)
+console.log(typeof booleanInNumber);  // "number"
+
+// "33" => 33 can change normal string into number
+//"33ad" =>NAN
+//true=>1 false=>0
+// 1=> true, 0=>false
+//" " => false
+//"komal" =>true
+//345 => "345" (convert to string)
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn) //true
+
+let it = ""
+let boolenIt = Boolean(it)
+console.log(boolenIt) //false for empty string
+
+// in strings
+
+let someNumber = 389
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof (stringNumber))//string
