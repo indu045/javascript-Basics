bdc15108080705acdd05817202148995453f05f5
indu045 <indujawla2005@gmail.com>
2025-06-19 07:19:30 +0000

    datatype comparision

---
+++ basics/datatypesComparsion.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+//true for both 
+//but always make sure when comparing anything 
+// their datatypes should be same
+
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>=0) //true
+// == and >< >= <= both works differently
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+// all false
+
+//=== (check both datatype and value)
+
+console.log("22" === 22) //false as datatype is different
+
+
